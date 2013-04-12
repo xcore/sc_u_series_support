546a89386f7155b450780bc70ccbeff70f64548c
Peter Hedinger <peter@xmos.com>
2013-04-12 10:43:53 +0100

    Fix documentation for XS1-U Series support

--- module_u_series_support/doc/u_series_support.rst
+++ module_u_series_support/doc/u_series_support.rst
@@ -1,2 +1,5 @@
+XS1-U Series Support Library
+============================
+
 .. code_rst_include:: src/u_series_support.h
 
--- module_u_series_support/src/u_series_support.h
+++ module_u_series_support/src/u_series_support.h
@@ -9,11 +9,12 @@
 /****
  * This file provides all the support required for in-device peripherals of
  * XS1-U Series devices:
+ * 
  *  - Analog to Digital Converters (adc.h)
  *  - Deep sleep support (TBD)
  *  - Watchdog timer (TBD)
  *
- *****/
+ ****/
 #include "adc.h"
 
 #endif // __u_series_support_h__
